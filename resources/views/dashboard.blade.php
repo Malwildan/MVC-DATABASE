adf4a1ee038eff2a51755143eee6150cba65068c
Akmal Wildan <muhammadakmal9187@gmail.com>
2026-05-16 04:02:21 +0700

    split branch: register and login focus

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,15 +10,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
-
-                    <div class="mt-4 flex flex-wrap gap-3">
-                        <a href="{{ route('users.index') }}" class="inline-flex items-center rounded-md border border-gray-300 px-3 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                            Buka MVC Users
-                        </a>
-                        <a href="{{ route('database.module') }}" class="inline-flex items-center rounded-md border border-gray-300 px-3 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                            Buka Database
-                        </a>
-                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,12 +15,6 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.*')">
-                        {{ __('Users') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('database.module')" :active="request()->routeIs('database.module')">
-                        {{ __('Implementasi Database') }}
-                    </x-nav-link>
                 </div>
             </div>
 
@@ -75,12 +69,6 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('users.index')" :active="request()->routeIs('users.*')">
-                {{ __('Users') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('database.module')" :active="request()->routeIs('database.module')">
-                {{ __('Implementasi Database') }}
             </x-responsive-nav-link>
         </div>
 
